2a2e7100e6ba9bb43001206d011dfce2a1bd9e24
irakli jiishkariani <irakli2692@gmail.com>
2017-11-30 20:20:09 +0400

    fenwick tree class

--- fenwick.js
+++ fenwick.js
@@ -1,19 +1,59 @@
 function sumParentIndex(index) {
   if (index === 0) return 0;
 
-  return x & (x - 1);
+  return index & (index - 1);
+}
+
+function changeParentIndex(index) {
+  return index + ( index & (-index) );
 }
 
 class FenwickTree {
   constructor(array) {
     this.tree = new Array(array.length).fill(0);
 
-
+    this.build(array);
   }
 
   private build(array) {
     this.tree[0] = array[0];
+
+    for (let i = 1; i < this.tree.length; i++) {
+      let parentIndex = sumParentIndex(i);
+
+      for (let j = parentIndex + 1; j <= i; j++) {
+        this.tree[i] += array[i];
+      }
+    }
   }
 
+  sumFromStart(endIndex) {
+    let sum = this.tree[0];
+
+    for (let i = endIndex; i > 0 ; i = sumParentIndex(i)) {
+      sum += this.tree[i];
+    }
+
+    return sum;
+  }
+
+  closedIntervalsum(start, end) {
+    return this.sumFromStart(end) - this.sumFromStart(start - 1);
+  }
+
+  add(index, value) {
+    if(!value) return;
+
+    if (index === 0) {
+      this.tree[0] += value;
+      return;
+    }
+
+    for (let i = index; i < this.tree.length; i = changeParentIndex(i)) {
+      this.tree[i] += value;
+    }
+  }
 
 }
+
+module.exports = FenwickTree;

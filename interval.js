387ead403b56b32483c6eaea6a3463adb27ad970
irakli jiishkariani <irakli2692@gmail.com>
2017-11-30 21:58:54 +0400

    refactor code. use ClosedInterval objects

---
+++ interval.js
@@ -0,0 +1,42 @@
+class ClosedInterval {
+  constructor(start, end) {
+    if (start > end) throw new Error('interval start is greater than end');
+
+    this.start = start;
+    this.end = end;
+  }
+
+  get middle() {
+    return this.start + Math.floor((this.end - this.start) / 2);
+  }
+
+  isInside(interval) {
+    return this.start >= interval.start && this.end <= interval.end;
+  }
+
+  isOutside(interval) {
+    return this.end < interval.start || this.start > interval.end;
+  }
+
+  isSingleElement() {
+    return this.start === this.end;
+  }
+
+  get leftHalfInterval() {
+    if (this.leftHalf) return this.leftHalf;
+
+    this.leftHalf = new ClosedInterval(this.start, this.middle);
+
+    return this.leftHalf;
+  }
+
+  get rightHalfInterval() {
+    if (this.rightHalf) return this.rightHalf;
+
+    this.rightHalf = new ClosedInterval(this.middle + 1, this.end);
+
+    return this.rightHalf;
+  }
+}
+
+module.exports = ClosedInterval;
--- segment.js
+++ segment.js
@@ -1,3 +1,5 @@
+const ClosedInterval = require('./interval');
+
 function leftChildIndex(index) {
   return index * 2 + 1;
 }
@@ -6,30 +8,54 @@ function rightChildIndex(index) {
   return index * 2 + 2;
 }
 
+function intervalMiddle(start, end) {
+  return start + Math.floor((end - start) / 2);
+}
+
 class SegmentTree {
   constructor(array) {
     this.tree = new Array(4 * array.length);
 
-    this.build(array, 0, this.tree.length - 1, 0); //whole array. sum node is at 0 index
+    this.build(
+      array,
+      new ClosedInterval(0, this.tree.length - 1),
+      0
+    ); //whole array. sum node is at 0 index
   }
 
-  private build(array, start, end, nodeIndex) {
-    if (start === end) {
-      this.tree[nodeIndex] = array[start];
+  private build(array, interval, nodeIndex) {
+    if (interval.isSingleElement()) {
+      this.tree[nodeIndex] = array[interval.start];
 
       return this.tree[nodeIndex];
     }
 
-    let middle = start + Math.floor((end - start) / 2);
-
     this.tree[nodeIndex] =
-      this.build(array, start, middle, leftChildIndex(nodeIndex)) +
-      this.build(array, middle + 1, end, rightChildIndex(nodeIndex));
+      this.build(array, interval.leftHalfInterval, leftChildIndex(nodeIndex)) +
+      this.build(array, interval.rightHalfInterval, rightChildIndex(nodeIndex));
 
     return this.tree[nodeIndex];
   }
 
   closedIntervalSum(start, end) {
-    
+
   }
+
+  private sumFromNodes(nodeInterval, queryInterval, nodeIndex) {
+    if (queryInterval.isInside(nodeInterval)) {
+      return this.tree[nodeIndex];
+    }
+
+    if (queryInterval.isOutside(nodeInterval)) {
+      return 0;
+    }
+
+    // query interval is partially inside node interval
+
+    return this.sumFromNodes(nodeInterval.leftHalfInterval, queryInterval, leftChildIndex(nodeIndex)) +
+      this.sumFromNodes(nodeInterval.rightHalfInterval, queryInterval, rightChildIndex(nodeIndex));
+  }
+
 }
+
+module.exports = SegmentTree;

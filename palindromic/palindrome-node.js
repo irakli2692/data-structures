505250403f7b93354761a510877c1271524b0fbe
irakli2692 <irakli2692@gmail.com>
2018-02-06 00:45:53 +0400

    palindrome node. not tested

--- palindromic/palindrome-node.js
+++ palindromic/palindrome-node.js
@@ -9,7 +9,56 @@ class PalindromeNode {
     }
 
     this.maxSuffixNode = maxSuffixNode
+    this.nextNodesMap = new Map()
   }
 
-  
+  get length() {
+    return this.text.length
+  }
+
+  get occurences() {
+    return this.startIndices.length
+  }
+
+  useForNewOccurence(newStartIndex) {
+    this.startIndices.push(newStartIndex)
+
+    this.lastStartIndex = newStartIndex
+  }
+
+  addLetter(letter, index, fullString) {
+    let previousIndex = this.previousIndex(index)
+
+    if (previousIndex >= 0 &&
+      fullString[previousIndex] === letter) {
+
+    }
+  }
+
+  createNodeIfNotExists(letter, index, fullString) {
+    let startIndex = this.previousIndex(index)
+
+    if (this.nextNodesMap.has(letter)) {
+      let nextNode = this.nextNodesMap.get(letter)
+
+      nextNode.useForNewOccurence(startIndex)
+
+      return nextNode
+    }
+
+    let suffixNode = this.getMaxSuffixNewNode(letter, index, fullString)
+
+    let newText = letter + this.text + letter
+
+    return new PalindromeNode(newText, startIndex, suffixNode)
+  }
+
+  previousIndex(index) {
+    return index - 1 - this.length
+  }
+
+  getMaxSuffixNewNode(letter, index, fullString) {
+    return this.maxSuffixNode.addLetter(letter, index, fullString)
+  }
+
 }

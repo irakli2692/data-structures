ff1e80fd5917ed30331257fdd4d1def8db08e218
irakli2692 <irakli2692@gmail.com>
2018-03-03 21:25:44 +0400

    fix imaginary node error

--- palindromic/imaginary-node.js
+++ palindromic/imaginary-node.js
@@ -1,24 +1,31 @@
 const PalindromeNode = require('./palindrome-node')
 
-const imaginaryNodeObject = {
-  maxSuffixNode: this,
-  nextNodesMap: new Map(),
-  get length() {
-    return -1
-  },
-  get occurences() {
-    return 0
-  },
-  useForNewOccurence() {},
-  isAppropriateNode() {
-    return true
-  },
-  newNodeText(letter) {
-    return letter
-  },
-  previousIndex(index) {
-    return index
-  }
+let imaginaryNode = Object.create(PalindromeNode.prototype)
+
+imaginaryNode.maxSuffixNode = imaginaryNode
+imaginaryNode.nextNodesMap = new Map()
+imaginaryNode.useForNewOccurence = function () {}
+imaginaryNode.isAppropriateNode = function () {
+  return true
+}
+imaginaryNode.newNodeText = function (letter) {
+  return letter
+}
+imaginaryNode.previousIndex = function (index) {
+  return index
 }
 
-module.exports = Object.create(PalindromeNode.prototype, imaginaryNodeObject)
+Object.defineProperties(imaginaryNode, {
+  length: {
+    get: function () {
+      return -1
+    }
+  },
+  occurences: {
+    get: function () {
+      return 0
+    }
+  }
+})
+
+module.exports = imaginaryNode
